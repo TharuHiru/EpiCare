aac94eb492c480d52e1e8b3215f86c584e53adb8
TharuHiru <147899885+TharuHiru@users.noreply.github.com>
2024-08-25 12:54:36 +0530

    Update dbh.inc.php

--- WEBPROJECT 4/includes/dbh.inc.php
+++ WEBPROJECT 4/includes/dbh.inc.php
@@ -1,12 +1,20 @@
 <?php
-$servername = "localhost:3307";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "dulmi_skincare";
+$servername = "epicare.mysql.database.azure.com";  // Azure MySQL server address
+$port = 3306;  // Default port for MySQL
+$dBUsername = "EpiAdmin";  // Your MySQL username
+$dBPassword = "WebAdEp12";  // Your MySQL password
+$dBName = "epicare_skincare";  // Your database name
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+// Create connection
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName, $port);
 
+// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+echo "Connected successfully";
+
+// Close connection
+mysqli_close($conn);
 ?>
