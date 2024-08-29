b3720a002148b8fe1301c90818ae01e8d72f0de7
TharuHiru <147899885+TharuHiru@users.noreply.github.com>
2024-08-29 08:16:29 +0530

    Update Usersignup.inc.php

--- WEBPROJECT 4/includes/Usersignup.inc.php
+++ WEBPROJECT 4/includes/Usersignup.inc.php
@@ -1,28 +1,7 @@
 <?php
 if (isset($_POST["submit"])) {
-    // Database configuration
-$host = 'epicare.mysql.database.azure.com';
-$port = 3306;
-$username = 'EpiAdmin';
-$password = 'WebAdEp12';
-$dbname = 'epicare_skincare';
+    require 'dbh.inc.php';
 
-// Path to your SSL certificate
-$ssl_ca = '/home/site/wwwroot/certs/ca-cert.pem'; // Ensure this path is correct
-
-// Create connection with SSL
-$conn = new mysqli();
-$conn->ssl_set(null, null, $ssl_ca, null, null);
-$conn->real_connect($host, $username, $password, $dbname, $port, null, MYSQLI_CLIENT_SSL);
-
-// Check connection
-if ($conn->connect_error) {
-    echo json_encode(['status' => 'error', 'message' => 'Database connection failed']);
-    exit();
-}
-
-if (isset($_POST["submit"])) {
-   
     $uid = $_POST['uid'];
     $email = $_POST['mail'];
     $pwd = $_POST['pwd'];
